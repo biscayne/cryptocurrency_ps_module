c813329e2899ad968e89efdba106d26cc1278325
Perry Peeters <perry@verspa.nl>
2017-04-07 19:08:37 +0200

    Add files via upload
    Added Dutch translation

---
+++ cryptocurrency/translations/nl.php
@@ -0,0 +1,19 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_4c11904ee0b793fc10046d533cae723e'] = 'Cryptocurrency';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_6053b56d3dc76a79dbed2524418eb37e'] = 'Accepteer betalingen voor uw producten in cryptocurrency';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_cbe0a99684b145e77f3e14174ac212e3'] = 'Weet u zeker dat u deze details wil verwijderen?';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_393fb8952f7b09d13fed1c7c92d94223'] = 'Wallet eigenaar en wallet details moeten geconfigureerd zijn voorda deze module gebruikt wordt.';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_a02758d758e8bec77a33d7f392eb3f8a'] = 'Geen valuta ingesteld voor deze module.';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_da1e66d02807830dcec1b3748c66b050'] = 'Bestand';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_c953261af8d55dd160305f972eb94967'] = 'bestaat al, copie afgebroken.';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_ec9b2158733547f6978afa125d50ba61'] = 'Copieeren mail template bestand mislukt.';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_edbeb8ad24993369742fb57068f54a30'] = 'Map';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_8c8e31876f98c0a1be2b10cde03d0d91'] = 'niet gevonden copieren van mail templates afgebroken.';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_f75fb049beb8d9c39e8cd0fdb812cce6'] = 'Wallet details zijn nodig.';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_a92a322f22c6ee873144fa9d5b7e14cc'] = 'Wallet eigenaar is nodig.';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_c888438d14855d7d96a2724ee9c306bd'] = 'Instellingen bijgewerkt';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_1260fe2d3b8abf7ecfbaa12b6aecd341'] = 'Deze module maakt cryptocurrency betalingen mogelijk.';
+$_MODULE['<{cryptocurrency}prestashop>cryptocurrency_0584e445d656b83b431227bb80ff0c30'] = 'Doneer';
